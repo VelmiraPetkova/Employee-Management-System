85f6b9bffeca4a102c608cc5706e48a26904968d
Velmira Georgieva <104137851+VelmiraPetkova@users.noreply.github.com>
2024-10-30 22:21:13 +0200

    Fix mocking 2

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,8 +1,11 @@
+from unittest.mock import patch
+
 import pytest
 from werkzeug.exceptions import BadRequest
 
 from managers.user import UserManager
 from models import UserModel, UserType
+from services.SES import SEService
 from tests.factories import UserFactory
 from tests.test_base import TestBase, generate_token
 
@@ -14,11 +17,38 @@ class TestUserManager(TestBase):
         assert result == user
 
 
+    @patch.object(SEService, 'verify_email_identity')
+    @patch.object(SEService, 'create_client')
+    def test_create_user_success(self,mock_create_client,mock_verify_email_identity):
+        user = UserModel.query.all()
+        assert len(user) == 0
+
+        headers = {
+            "Content-Type": "application/json"
+        }
+
+        test_mail = "velmira@gmail.com"
+        data= {
+                "name": "Test Test",
+                "civil_number": "9408303257",
+                "phone": "0811111111",
+                "iban": "BG70BNBG47622021445672",
+                "email": test_mail,
+                "password": "Welmira2023-"
+        }
+        res = self.client.post("/register", json=data, headers=headers)
+        mock_create_client.assert_called_once()
+        mock_verify_email_identity.assert_called_once_with(test_mail)
+        user = UserModel.query.all()
+        assert len(user) == 1
+
     def test_check_manager_success(self):
         #user = UserFactory(id=1)
         user_manager = UserFactory(id=2)
         result =  UserModel.query.filter_by(id=user_manager.id).first()
         assert result ==user_manager
+
+
 
     def test_assign_manager_success(self):
         user=UserFactory(id=7)
@@ -34,8 +64,6 @@ class TestUserManager(TestBase):
         data = {"manager": user_manager.id}
         res = self.client.put(f"/addmanager/user/{user.id}", json=data, headers=headers)
         assert res.status_code == 200
-
-
 
     def test_assign_manager_failure(self):
         user=UserFactory(id=10)
@@ -53,26 +81,6 @@ class TestUserManager(TestBase):
         res = self.client.put(f"/addmanager/user/{user.id}", json=data, headers=headers)
         assert res.status_code == 400
         assert res.json=={'message': 'No such manager exists'}
-
-    def test_create_user_success(self):
-        user = UserModel.query.all()
-        assert len(user) == 0
-
-        headers = {
-            "Content-Type": "application/json"
-        }
-
-        data= {
-                "name": "Test Test",
-                "civil_number": "9408303257",
-                "phone": "0811111111",
-                "iban": "BG70BNBG47622021445672",
-                "email": "velmira@gmail.com",
-                "password": "Welmira2023-"
-        }
-        res = self.client.post("/register", json=data, headers=headers)
-        user = UserModel.query.all()
-        assert len(user) == 1
 
 
     def test_create_user_failure(self):
